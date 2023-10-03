3afbd26871b19fbd593b6146e997fae1993f509b
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-10-02 20:28:58 -0400

    Style feed sidebar

--- features/feed-sidebar/components/sidebar-spaces.tsx
+++ features/feed-sidebar/components/sidebar-spaces.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Anchor, Stack } from '@mantine/core';
+import { Anchor, Stack, Text } from '@mantine/core';
 import Link from 'next/link';
 import { Database } from '@/lib/database';
 import { DEFAULT_SORT } from '@/lib/constants';
@@ -11,16 +11,19 @@ type SidebarSpacesProps = {
 
 export function SidebarSpaces({ spaces }: SidebarSpacesProps) {
   return (
-    <Stack gap="xs">
-      {spaces.map((space) => (
-        <Anchor
-          key={space.id}
-          component={Link}
-          href={`/spaces/${space.id}/${space.name}/${DEFAULT_SORT}`}
-        >
-          {space.display_name}
-        </Anchor>
-      ))}
-    </Stack>
+    <>
+      <Text>My Spaces</Text>
+      <Stack gap="xs" mt="xs">
+        {spaces.map((space) => (
+          <Anchor
+            key={space.id}
+            component={Link}
+            href={`/spaces/${space.id}/${space.name}/${DEFAULT_SORT}`}
+          >
+            {space.display_name}
+          </Anchor>
+        ))}
+      </Stack>
+    </>
   );
 }

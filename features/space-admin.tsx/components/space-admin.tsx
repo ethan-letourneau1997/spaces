26025d90b63f154d25d258720ff148393e816de5
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-10-09 20:20:57 -0400

    Prefetch tab data

--- features/space-admin.tsx/components/space-admin.tsx
+++ features/space-admin.tsx/components/space-admin.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { Anchor, Card, Container, Tabs } from '@mantine/core';
-import Link from 'next/link';
+import { Card, Container, Tabs } from '@mantine/core';
 
+import { useRouter } from 'next/navigation';
 import { Database } from '@/lib/database';
-import classes from './space-admin.module.css';
 
 type SpaceAdminProps = {
   space: Database['public']['Tables']['community']['Row'];
@@ -13,35 +12,22 @@ type SpaceAdminProps = {
 };
 
 export function SpaceAdmin({ tab, space, tabContent }: SpaceAdminProps) {
+  const router = useRouter();
+
+  const prefetchValue = tab === 'settings' ? 'metrics' : 'settings';
+
+  router.prefetch(`/spaces/${space.id}/${space.name}/admin/${prefetchValue}`);
+
   return (
     <Container size="sm">
       <Card>
-        <Tabs defaultValue="profile" value={tab}>
+        <Tabs
+          value={tab}
+          onChange={(value) => router.push(`/spaces/${space.id}/${space.name}/admin/${value}`)}
+        >
           <Tabs.List>
-            <Tabs.Tab value="settings">
-              <Anchor
-                classNames={{
-                  root: classes.tabAnchor,
-                }}
-                c="gray.0"
-                component={Link}
-                href={`/spaces/${space.id}/${space.name}/admin/settings`}
-              >
-                Settings
-              </Anchor>
-            </Tabs.Tab>
-            <Tabs.Tab value="metrics">
-              <Anchor
-                classNames={{
-                  root: classes.tabAnchor,
-                }}
-                c="gray.0"
-                component={Link}
-                href={`/spaces/${space.id}/${space.name}/admin/metrics`}
-              >
-                Metrics
-              </Anchor>
-            </Tabs.Tab>
+            <Tabs.Tab value="settings">Settings</Tabs.Tab>
+            <Tabs.Tab value="metrics">Metrics</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel value="settings">{tabContent}</Tabs.Panel>
           <Tabs.Panel value="metrics">{tabContent}</Tabs.Panel>

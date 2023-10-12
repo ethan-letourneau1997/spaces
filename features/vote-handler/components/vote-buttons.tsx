a6901029436bdba61f9213198ead39142218d3c3
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-10-11 20:15:30 -0400

    Style vote buttons

--- features/vote-handler/components/vote-buttons.tsx
+++ features/vote-handler/components/vote-buttons.tsx
@@ -3,32 +3,31 @@
 import { BiDownvote, BiSolidDownvote, BiSolidUpvote, BiUpvote } from 'react-icons/bi';
 import { Box, em } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
-import classes from './vote-buttons.module.css';
 
 export function UpvoteButton() {
   const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
   return (
-    <Box className={classes.voteButtonWrapper}>
-      <BiUpvote className={classes.voteButton} size={isMobile ? 14 : 16} />
+    <Box className="hover:text-yellow-6 text-gray-5">
+      <BiUpvote size={isMobile ? 14 : 16} />
     </Box>
   );
 }
 
 export function UpvotedButton() {
   const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
-  return <BiSolidUpvote className={classes.votedButton} size={isMobile ? 14 : 16} />;
+  return <BiSolidUpvote className="text-yellow-6" size={isMobile ? 14 : 16} />;
 }
 
 export function DownvoteButton() {
   const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
   return (
-    <Box className={classes.voteButtonWrapper}>
-      <BiDownvote className={classes.voteButton} size={isMobile ? 14 : 16} />
+    <Box className="hover:text-yellow-6 text-gray-5">
+      <BiDownvote size={isMobile ? 14 : 16} />
     </Box>
   );
 }
 
 export function DownvotedButton() {
   const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
-  return <BiSolidDownvote className={classes.votedButton} size={isMobile ? 14 : 16} />;
+  return <BiSolidDownvote className="text-yellow-6" size={isMobile ? 14 : 16} />;
 }

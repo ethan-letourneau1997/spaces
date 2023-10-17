4d01e577b1d4608ffd004575a667dbaf5dc98932
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-10-16 22:16:55 -0400

    Add comment count to post footer

--- features/post/components/post-footer.tsx
+++ features/post/components/post-footer.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { Collapse, UnstyledButton, Text } from '@mantine/core';
+import { Collapse, UnstyledButton } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { FaCommentAlt } from 'react-icons/fa';
 import { BsFillReplyFill } from 'react-icons/bs';
 import { ImCross } from 'react-icons/im';
 import { RootCommentInput, RootCommentModal } from '@/features/root-comment-input';
 import { VoteButtons } from '@/features/vote-handler';
 import { Database } from '@/lib/database';
 import { PostSaveButton } from '@/features/post-save-button';
+import { PostCommentCount } from '@/components/post-comment-count';
 
 type PostFooterProps = {
   postVotes: number;
@@ -24,16 +24,7 @@ export function PostFooter({ postVotes, userVote, post, commentCount }: PostFoot
     <>
       <div className="flex gap-4 mt-2 ">
         <VoteButtons horizontal totalVotes={postVotes || 0} userVote={userVote || 0} post={post} />
-        <div className="flex items-center gap-1.5 ">
-          <UnstyledButton className="flex items-center" c="dark.2" pt={1}>
-            <FaCommentAlt size={14} />
-          </UnstyledButton>
-
-          <Text size="sm" c="dark.2" fw={700} className="text-sm ">
-            {commentCount || 0} comment{commentCount !== 1 || !commentCount ? 's' : ''}
-          </Text>
-        </div>
-
+        <PostCommentCount count={commentCount} />
         <UnstyledButton
           className="!text-sm hover:!text-dark-0 flex items-center gap-1"
           py={2}
@@ -45,9 +36,7 @@ export function PostFooter({ postVotes, userVote, post, commentCount }: PostFoot
           {opened ? <ImCross size={10} /> : <BsFillReplyFill size={18} className="mb-0.5" />}
           {opened ? 'Cancel' : 'Reply'}
         </UnstyledButton>
-
         <PostSaveButton post={post} />
-
         <RootCommentModal post={post} />
       </div>
       <Collapse in={opened} pt="xs">
